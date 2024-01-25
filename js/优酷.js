d430d48e0deb41a2db93f09a41af8dcb1b79a123
gly <11745379+glytv@user.noreply.gitee.com>
2024-01-25 08:53:28 +0000

    update js/优酷.js.
    Signed-off-by: gly <11745379+glytv@user.noreply.gitee.com>

--- js/优酷.js
+++ js/优酷.js
@@ -1,14 +1,20 @@
 var rule = {
-    title: '优酷',
-    host: 'https://www.%79%6f%75%6b%75.com',
-    homeUrl: '',
-    searchUrl: 'https://search.%79%6f%75%6b%75.com/api/search?pg=fypage&keyword=**',
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 1,
-    multi: 1,
-    url: '/category/data?optionRefresh=1&pageNo=fypage&params=fyfilter',
-    filter_url: '{{fl}}',
+    title:'优酷',
+    host:'https://www.%79%6f%75%6b%75.com',
+    homeUrl:'',
+    searchUrl:'https://search.%79%6f%75%6b%75.com/api/search?pg=fypage&keyword=**',
+    searchable:2,
+    quickSearch:0,
+    filterable:1,
+    multi:1,
+    // 分类链接fypage参数支持1个()表达式
+    url:'/category/data?optionRefresh=1&pageNo=fypage&params=fyfilter',
+    // url:'/category/data?pageNo=fypage&params=fyfilter',
+    // filter_url:'&u=fyarea&s=fyyear={{fl.order}}',
+    // filter_url:'{{fl|safe}}',
+    filter_url:'{{fl}}',
+    // filter_url:'{{fl}}',
+    // filter_url:'{{fl}}',
     filter: { "电视剧":[
     { "key": "main_area", "name": "全部地区","value": [{ "n": "全部地区", "v": "" }, { "n": "内地剧", "v": "中国内地" }, { "n": "港剧", "v": "中国香港" }, { "n": "台剧", "v": "中国台湾" }, { "n": "韩剧", "v": "韩国" }, { "n": "美剧", "v": "美国" }, { "n": "英剧", "v": "英国" }, { "n": "日剧", "v": "日本" }, { "n": "泰剧", "v": "泰国" }] }, 
     { "key": "tags", "name": "全部类型", "value": [{ "n": "全部类型", "v": "" }, { "n": "青春", "v": "青春" }, { "n": "古装", "v": "古装" }, { "n": "爱情", "v": "爱情" }, { "n": "都市", "v": "都市" }, { "n": "喜剧", "v": "喜剧,搞笑" }, { "n": "战争", "v": "战争" }, { "n": "军旅", "v": "军旅" }, { "n": "谍战", "v": "谍战" }, { "n": "偶像", "v": "偶像" }, { "n": "警匪", "v": "警匪" }, { "n": "冒险", "v": "冒险" }, { "n": "穿越", "v": "穿越" }, { "n": "仙侠", "v": "仙侠" }, { "n": "武侠", "v": "武侠" }, { "n": "悬疑", "v": "悬疑" }, { "n": "罪案", "v": "罪案" }, { "n": "家庭", "v": "家庭" }, { "n": "历史", "v": "历史" }, { "n": "年代", "v": "年代" }, { "n": "农村", "v": "农村" }] },
@@ -24,7 +30,7 @@ var rule = {
         { "key": "year", "name": "全部年份", "value": [{ "n": "全部年份", "v": "" }, { 'n': '2023', 'v': '2023' }, { 'n': '2022', 'v': '2022' }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }, { "n": "2019", "v": "2019" }, { "n": "2018", "v": "2018" }, { "n": "2017", "v": "2017" }, { "n": "2016", "v": "2016" }, { "n": "2015", "v": "2015" }, { "n": "2014-2010", "v": "2010-2014" }, { "n": "2009-2000", "v": "2000-2009" }, { "n": "90年代", "v": "1990-1999" }, { "n": "80年代", "v": "1980-1989" }, { "n": "70年代", "v": "1970-1979" }, { "n": "更早", "v": "-1969" }] }, 
         { "key": "pay_type", "name": "付费类型", "value": [{ "n": "付费类型", "v": "" }, { "n": "免费", "v": "0" }, { "n": "会员", "v": "2" }, { "n": "点播", "v": "1" }] }, 
         { "key": "sort", "name": "综合排序", "value": [{ "n": "综合排序", "v": "" }, { "n": "热度最高", "v": "7" }, { "n": "最多播放", "v": "2" }, { "n": "最新上线", "v": "1" }, { "n": "最好评", "v": "3" }] }, 
-        { "key": "tag_label_name", "name": "为你推荐", "value": [{ "n": "为你推荐", "v": "" }, { "n": "高分必看", "v": "高分必看" }, { "n": "卖座电影", "v": "卖座电影" }, { "n": "小说改编", "v": "小说改编" }, { "n": "铁血硬汉", "v": "铁血硬汉" }, { "n": "视效大片", "v": "视效大片" }, { "n": "漫画改编", "v": "漫画改编" }, { "n": "绝地求生", "v": "绝地求生" }, { "n": "真人真事改编", "v": "真人真事改编" }, { "n": "范伟", "v": "范伟" }, { "n": "火爆枪战", "v": "火爆枪战" }, { "n": "影史经典", "v": "影史经典" }, { "n": "拯救世界", "v": "拯救世界" }, { "n": "万茜", "v": "万茜" }, { "n": "马思纯", "v": "马思纯" }, { "n": "王俊凯", "v": "王俊凯" }, { "n": "豪华阵容", "v": "豪华阵容" }, { "n": "李玉", "v": "李玉" }, { "n": "无厘头喜剧", "v": "无厘头喜剧" }, { "n": "超能力", "v": "超能力" }, { "n": "欢喜搭档", "v": "欢喜搭档" }] }], 
+        ], 
         
         "综艺": [
         { "key": "main_area", "name": "全部地区", "value": [{ "n": "全部地区", "v": "" }, { "n": "内地", "v": "中国内地" }, { "n": "中国台湾", "v": "中国台湾" }, { "n": "美国", "v": "美国" }, { "n": "英国", "v": "英国" }] }, 
@@ -33,7 +39,7 @@ var rule = {
         { "key": "status", "name": "全部规格", "value": [{ "n": "全部规格", "v": "" }, { "n": "优酷自制", "v": "2" }, { "n": "优酷独播", "v": "1" }, { "n": "电视综艺", "v": "6" }, { "n": "已完结", "v": "3" }, { "n": "即将上线", "v": "4" }] }, 
         { "key": "pay_type", "name": "付费类型", "value": [{ "n": "付费类型", "v": "" }, { "n": "免费", "v": "0" }, { "n": "VIP", "v": "2" }, { "n": "付费", "v": "1" }] }, 
         { "key": "sort", "name": "热度最高", "value": [{ "n": "热度最高", "v": "" }, { "n": "最新更新", "v": "8" }, { "n": "最近开播", "v": "9" }, { "n": "最多评论", "v": "4" }] }, 
-        { "key": "tag_label_name", "name": "为你推荐", "value": [{ "n": "为你推荐", "v": "" }, { "n": "舞台竞演", "v": "舞台竞演" }, { "n": "刘雨昕", "v": "刘雨昕" }, { "n": "王一博", "v": "王一博" }, { "n": "韩庚", "v": "韩庚" }, { "n": "李承铉", "v": "李承铉" }, { "n": "户外竞技", "v": "户外竞技" }, { "n": "浙江卫视", "v": "浙江卫视" }, { "n": "东方卫视", "v": "东方卫视" }, { "n": "岳云鹏", "v": "岳云鹏" }, { "n": "王迅", "v": "王迅" }, { "n": "杨迪", "v": "杨迪" }, { "n": "杨超越", "v": "杨超越" }, { "n": "黄明昊", "v": "黄明昊" }, { "n": "郭京飞", "v": "郭京飞" }, { "n": "欧阳娜娜", "v": "欧阳娜娜" }, { "n": "德云社", "v": "德云社" }, { "n": "郑恺", "v": "郑恺" }, { "n": "华少", "v": "华少" }, { "n": "郭德纲", "v": "郭德纲" }, { "n": "贾玲", "v": "贾玲" }] }], 
+        ], 
         
         "动漫": [
         { "key": "sort", "name": "综合排序", "value": [{ "n": "综合排序", "v": "" }, { "n": "最多播放", "v": "2" }, { "n": "最好评", "v": "3" }, { "n": "最新上线", "v": "1" }] }, 
@@ -42,7 +48,7 @@ var rule = {
         { "key": "completed", "name": "连载情况", "value": [{ "n": "连载情况", "v": "" }, { "n": "更新中", "v": "0" }, { "n": "已完结", "v": "1" }] }, 
         { "key": "pay_type", "name": "付费类型", "value": [{ "n": "付费类型", "v": "" }, { "n": "免费", "v": "0" }, { "n": "会员", "v": "2" }] }, 
         { "key": "year", "name": "全部年份", "value": [{ "n": "全部年份", "v": "" }, { 'n': '2023', 'v': '2023' }, { 'n': '2022', 'v': '2022' }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }, { "n": "2019", "v": "2019" }, { "n": "2018", "v": "2018" }, { "n": "2017", "v": "2017" }, { "n": "2016", "v": "2016" }, { "n": "2015", "v": "2015" }, { "n": "2014", "v": "2014" }, { "n": "2013", "v": "2013" }, { "n": "2012", "v": "2012" }, { "n": "2011", "v": "2011" }, { "n": "00年代", "v": "2000-2010" }, { "n": "更早", "v": "-1999" }] }, 
-        { "key": "tag_label_name", "name": "为你推荐", "value": [{ "n": "为你推荐", "v": "" }, { "n": "3D动画", "v": "3D动画" }, { "n": "搞笑日常", "v": "搞笑日常" }, { "n": "大男主", "v": "大男主" }, { "n": "高燃", "v": "高燃" }, { "n": "萌系", "v": "萌系" }, { "n": "修真", "v": "修真" }, { "n": "怀旧", "v": "怀旧" }, { "n": "超级英雄", "v": "超级英雄" }, { "n": "水树奈奈", "v": "水树奈奈" }, { "n": "张杰", "v": "张杰" }, { "n": "石田彰", "v": "石田彰" }, { "n": "守护地球", "v": "守护地球" }, { "n": "圆谷", "v": "圆谷" }, { "n": "山新", "v": "山新" }, { "n": "复仇", "v": "复仇" }, { "n": "废柴逆袭", "v": "废柴逆袭" }, { "n": "侦探", "v": "侦探" }, { "n": "无厘头", "v": "无厘头" }, { "n": "青山刚昌", "v": "青山刚昌" }, { "n": "悬疑", "v": "悬疑" }] }], 
+        ], 
         
         "少儿": [
         { "key": "main_area", "name": "全部地区", "value": [{ "n": "全部地区", "v": "" }, { "n": "中国", "v": "中国,香港,台湾" }, { "n": "美国", "v": "美国" }, { "n": "英国", "v": "英国" }, { "n": "其他国家", "v": "其他" }] }, 
@@ -103,5 +109,24 @@ var rule = {
                     { "key": "tags", "name": "全部类型", "value": [{ "n": "全部类型", "v": "" }, { "n": "资讯", "v": "资讯" }, { "n": "解说", "v": "解说" }, { "n": "主播", "v": "主播" }, { "n": "沙盒", "v": "沙盒" }, { "n": "益智", "v": "益智" }, { "n": "生存", "v": "生存" }, { "n": "电子竞技", "v": "电子竞技" }, { "n": "单机主机", "v": "单机主机" }, { "n": "真人秀", "v": "真人秀" }, { "n": "动画", "v": "动画" }, { "n": "盘点", "v": "盘点" }, { "n": "游戏文化", "v": "游戏文化" }, { "n": "游戏节目", "v": "游戏节目" }, { "n": "评测", "v": "评测" }, { "n": "射击", "v": "射击" }, { "n": "策略", "v": "策略" }, { "n": "冒险", "v": "冒险" }, { "n": "即时战略", "v": "即时战略" }, { "n": "卡牌", "v": "卡牌" }, { "n": "角色扮演", "v": "角色>扮演" }] }, 
                     { "key": "sort", "name": "综合排序", "value": [{ "n": "综合排序", "v": "" }, { "n": "最多播放", "v": "2" }, { "n": "最好评", "v": "3" }, { "n": "最新上线", "v": "1" }] }] },
 
-
+    headers:{
+        'User-Agent':'PC_UA',
+        'Cookie':'cna=VvNvGX3e0ywCAavVEXlnA2bg; __ysuid=1626676228345Rl1; __ayft=1652434048647; __arycid=dm-1-00; __arcms=dm-1-00; __ayvstp=85; __arpvid=1667204023100cWWdgM-1667204023112; __ayscnt=10; __aypstp=60; isg=BBwcqxvvk3BxkWQGugbLpUSf7TrOlcC_U7GAj_YdfYfvQbzLHqYGT4Hgp6m5TvgX; tfstk=c3JOByYUH20ilVucLOhh0pCtE40lZfGc-PjLHLLfuX7SWNyAiQvkeMBsIw7PWDC..; l=eBQguS-PjdJFGJT-BOfwourza77OSIRA_uPzaNbMiOCPOb1B5UxfW6yHp4T6C3GVhsGJR3rp2umHBeYBqQd-nxvOF8qmSVDmn',
+    },
+    timeout:5000,
+    class_name:'电视剧&电影&综艺&动漫&少儿&纪录片&文化&亲子&教育&搞笑&生活&体育&音乐&游戏',
+    class_url:'电视剧&电影&综艺&动漫&少儿&纪录片&文化&亲子&教育&搞笑&生活&体育&音乐&游戏',
+    limit:20,
+    play_parse:true,
+    // 手动调用解析请求json的url,此lazy不方便
+    // lazy:'js:print(input);fetch_params.headers["user-agent"]=MOBILE_UA;let html=request(input);let rurl=html.match(/window\\.open\\(\'(.*?)\',/)[1];rurl=urlDeal(rurl);input={parse:1,url:rurl};',
+    lazy:'js:input={parse:1,jx:1,url:input};',
+    // 推荐:'.list_item;img&&alt;img&&src;a&&Text;a&&data-float',
+    // 一级:'json:data.filterData.listData;title;img;subTitle;videoLink;summary',
+    一级:'',
+    一级:'js:let d=[];MY_FL.type=MY_CATE;let fl=stringify(MY_FL);fl=encodeUrl(fl);input=input.split("{")[0]+fl;if(MY_PAGE>1){let old_session=getItem("yk_session_"+MY_CATE,"{}");if(MY_PAGE===2){input=input.replace("optionRefresh=1","session="+encodeUrl(old_session))}else{input=input.replace("optionRefresh=1","session="+encodeUrl(old_session))}}let html=fetch(input,fetch_params);try{html=JSON.parse(html);let lists=html.data.filterData.listData;let session=html.data.filterData.session;session=stringify(session);if(session!==getItem("yk_session_"+MY_CATE,"{}")){setItem("yk_session_"+MY_CATE,session)}lists.forEach(function(it){let vid;if(it.videoLink.includes("id_")){vid=it.videoLink.split("id_")[1].split(".html")[0]}else{vid="msearch:"}d.push({title:it.title,img:it.img,desc:it.summary,url:"https://search.youku.com/api/search?appScene=show_episode&showIds="+vid,content:it.subTitle})})}catch(e){log("一级列表解析发生错误:"+e.message)}setResult(d);',
+    二级:'',
+    二级:'js:var d=[];VOD={};let html=request(input);let json=JSON.parse(html);if(/keyword/.test(input)){input="https://search.youku.com/api/search?appScene=show_episode&showIds="+json.pageComponentList[0].commonData.showId;json=JSON.parse(fetch(MY_URL,fetch_params))}let video_lists=json.serisesList;var name=json.sourceName;if(/优酷/.test(name)&&video_lists.length>0){let ourl="https://v.youku.com/v_show/id_"+video_lists[0].videoId+".html";let _img=video_lists[0].thumbUrl;let html=fetch(ourl,{headers:{Referer:"https://v.youku.com/","User-Agent":PC_UA}});let json=/__INITIAL_DATA__/.test(html)?html.split("window.__INITIAL_DATA__ =")[1].split(";")[0]:"{}";if(json==="{}"){log("触发了优酷人机验证");VOD.vod_remarks=ourl;VOD.vod_pic=_img;VOD.vod_name=video_lists[0].title.replace(/(\\d+)/g,"");VOD.vod_content="触发了优酷人机验证,本次未获取详情,但不影响播放("+ourl+")"}else{try{json=JSON.parse(json);let data=json.data.data;let data_extra=data.data.extra;let img=data_extra.showImgV;let model=json.data.model;let m=model.detail.data.nodes[0].nodes[0].nodes[0].data;let _type=m.showGenre;let _desc=m.updateInfo||m.subtitle;let JJ=m.desc;let _title=m.introTitle;VOD.vod_pic=img;VOD.vod_name=_title;VOD.vod_type=_type;VOD.vod_remarks=_desc;VOD.vod_content=JJ}catch(e){log("海报渲染发生错误:"+e.message);print(json);VOD.vod_remarks=name}}}if(!/优酷/.test(name)){VOD.vod_content="非自家播放源,暂无视频简介及海报";VOD.vod_remarks=name}function adhead(url){return urlencode(url)}play_url=play_url.replace("&play_url=","&type=json&play_url=");video_lists.forEach(function(it){let url="https://v.youku.com/v_show/id_"+it.videoId+".html";if(it.thumbUrl){d.push({desc:it.showVideoStage?it.showVideoStage.replace("期","集"):it.displayName,pic_url:it.thumbUrl,title:it.title,url:play_url+adhead(url)})}else if(name!=="优酷"){d.push({title:it.displayName?it.displayName:it.title,url:play_url+adhead(it.url)})}});VOD.vod_play_from=name;VOD.vod_play_url=d.map(function(it){return it.title+"$"+it.url}).join("#");',
+    搜索:'',
+    搜索:'js:var d=[];let html=request(input);let json=JSON.parse(html);json.pageComponentList.forEach(function(it){if(it.hasOwnProperty("commonData")){it=it.commonData;d.push({title:it.titleDTO.displayName,img:it.posterDTO.vThumbUrl,desc:it.stripeBottom,content:it.updateNotice+" "+it.feature,url:"https://search.youku.com/api/search?appScene=show_episode&showIds="+it.showId+"&appCaller=h5"})}});setResult(d);',
 }
